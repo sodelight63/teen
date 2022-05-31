b2885023e2a0302f3bd6e461e99b621a7689772e
sodelight63 <624234022@parichat.skru.ac.th>
2022-05-31 20:46:32 +0700

    v.1

---
+++ index.php
@@ -0,0 +1,33 @@
+<?Php
+require_once('config/connect_db.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>หน้าแรก</title>
+</head>
+
+<body>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mt-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>ข้อมูลลูกค้า
+                            <a href="insert.php" class="btn btn-primary float-end">เพิ่มข้อมูลลูกค้า</a>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="js/bootstrap.min.js"></script>
+
+</html>
